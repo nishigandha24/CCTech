defee734e8fc9548d5e5a3a676439bd6278995f6
Nisheegandha Bhookan <nishigandhabhukan99@gmail.com>
2020-07-01 18:25:27 +0530

    Update Polygon.cpp

--- Polygon.cpp
+++ Polygon.cpp
@@ -2,31 +2,31 @@
 using namespace std;
 
 struct Point {
-   float x, y;
+ float x, y;
 };
 
 struct line {
-   Point p1, p2;
+ Point p1, p2;
 };
 
 bool onLine(line l1, Point p) {        //check whether p is on the line or not
-   if(p.x <= max(l1.p1.x, l1.p2.x) && p.x <= min(l1.p1.x, l1.p2.x) &&
-      (p.y <= max(l1.p1.y, l1.p2.y) && p.y <= min(l1.p1.y, l1.p2.y)))
-      return true;
+ if(p.x <= max(l1.p1.x, l1.p2.x) && p.x <= min(l1.p1.x, l1.p2.x) &&
+  (p.y <= max(l1.p1.y, l1.p2.y) && p.y <= min(l1.p1.y, l1.p2.y)))
+  return true;
 
-   return false;
+return false;
 }
 
 int direction(Point a, Point b, Point c) {
-   int val = (b.y-a.y)*(c.x-b.x)-(b.x-a.x)*(c.y-b.y);
-   if (val == 0)
+ int val = (b.y-a.y)*(c.x-b.x)-(b.x-a.x)*(c.y-b.y);
+ if (val == 0)
       return 0;           //colinear
-   else if(val < 0)
+    else if(val < 0)
       return 2;          //anti-clockwise direction
       return 1;          //clockwise direction
-   }
+    }
 
-   bool isIntersect(line l1, line l2) {
+    bool isIntersect(line l1, line l2) {
    //four direction for two lines and points of other line
       int dir1 = direction(l1.p1, l1.p2, l2.p1);
       int dir2 = direction(l1.p1, l1.p2, l2.p2);
@@ -36,18 +36,18 @@ int direction(Point a, Point b, Point c) {
       if(dir1 != dir2 && dir3 != dir4)
       return true;           //they are intersecting
    if(dir1==0 && onLine(l1, l2.p1))        //when p2 of line2 are on the line1
-      return true;
+    return true;
    if(dir2==0 && onLine(l1, l2.p2))         //when p1 of line2 are on the line1
-      return true;
+    return true;
    if(dir3==0 && onLine(l2, l1.p1))       //when p2 of line1 are on the line2
-      return true;
+    return true;
    if(dir4==0 && onLine(l2, l1.p2)) //when p1 of line1 are on the line2
-      return true;
-   return false;
+    return true;
+  return false;
 }
 
 bool checkInside(Point poly[], int n, Point p) {
-   if(n < 3)
+ if(n < 3)
       return false;                  //when polygon has less than 3 edge, it is not polygon
    line exline = {p, {9999, p.y}};   //create a point at infinity, y is same as point p
    int count = 0;
@@ -55,28 +55,26 @@ bool checkInside(Point poly[], int n, Point p) {
    do {
       line side = {poly[i], poly[(i+1)%n]};     //forming a line from two consecutive points of poly
       if(isIntersect(side, exline)) {          //if side is intersects exline
-         if(direction(side.p1, p, side.p2) == 0)
-            return onLine(side, p);
-         count++;
-      }
-      i = (i+1)%n;
-   } while(i != 0);
+       if(direction(side.p1, p, side.p2) == 0)
+        return onLine(side, p);
+      count++;
+    }
+    i = (i+1)%n;
+  } while(i != 0);
       return count&1;             //when count is odd
-   }
+    }
 
-   int main() {
-   // line polygon = {{{0,0},{10,0}},{{10,0},{10,10}},{{10,10},{0,10}},{{0,10},{0,0}}};
-
-     Point polygon[] = {{0, 0}, {10, 0}, {10, 10}, {0, 10}};
-     Point p = {5, 3}; //Point will lies inside
+    int main() {
+     Point polygon[] = {{1,0}, {8,3}, {8,8}, {1,5}};
+     Point p = {3,5}; //Point will lies inside
      int n = 4;
 
-     // Point polygon[] = {{-3, 2}, {-2, -0.8}, {0, 1.2}, {2.2, 0}};
+     // Point polygon[] = {{-3, 2}, {-2, -0.8}, {0, 1.2}, {2.2, 0}, {2,4.5}};
      //Point p = {0, 0};  //Point will lies outside
-     //int n = 4;
+     //int n = 5;
 
      if(checkInside(polygon, n, p))
          cout << "True"; //Point lies inside.
-      else
+       else
          cout << "False"; //Point lies outside.
-   }
+     }
